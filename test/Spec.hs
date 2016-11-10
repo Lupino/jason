d7585fbbffeb4bc88c414498ddc276c354939557
Lupino <lmjubuntu@gmail.com>
2016-11-10 11:29:24 +0800

    update test

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,19 +3,25 @@ import           Data.Aeson                 (encode, toEncoding)
 import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Text                  (Text)
 import           Jason
+import           Prelude                    hiding (head)
 
 main :: IO ()
-main = do
-  let v = renderJasonM True $ do
-            arrayParent "aa" $ do
-              "cc"
-              "dd"
-              leaf $ do
-                parent "ee" "ff"
-                parent "gg" "hh"
-              label "cckk" mempty
-              label "cckk" $ do
-                font "cc"
-                size "100"
+main = B.putStrLn . encode $ renderJason app
 
-  B.putStrLn $ encode v
+app :: Jason
+app = jason $ do
+  head $ do
+    title "Hello World"
+    description "This app displays 'Hello World' on the screen"
+    styles $
+      styleRow "styled_row" $ do
+        font "HelveticaNeue"
+        size "20"
+        color "#ff0000"
+        padding "10"
+  body $
+    sections $ do
+      items $ do
+        label "This is row 1" $ class_ "styled_row"
+        label "This is row 2" $ class_ "styled_row"
+        label "This is row 3" $ class_ "styled_row"
